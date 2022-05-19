aed68305744ab08e78c74379a30ab384591764f1
DomerJi <pengshuaiji@outlook.com>
2022-05-19 09:26:42 +0800

    two commit

---
+++ port/serial-assistant-master (1)/serial-assistant-master/app/src/main/java/com/sysout/app/serial/AppApplication.java
@@ -0,0 +1,32 @@
+package com.sysout.app.serial;
+
+import android.app.Application;
+
+import com.blankj.utilcode.util.LogUtils;
+import com.sysout.app.serial.utils.DBUtils;
+import com.tencent.bugly.Bugly;
+import com.tencent.mmkv.MMKV;
+
+public class AppApplication extends Application {
+
+    private static Application sInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sInstance = this;
+        DBUtils.getInstance().initDB(this);
+        MMKV.initialize(this.getFilesDir().getAbsolutePath() + "/mmkv");
+        Bugly.init(this.getApplicationContext(), "525d37cba2", BuildConfig.DEBUG);
+    }
+
+    /**
+     * 获得当前app运行的Application
+     */
+    public static Application getInstance() {
+        if (sInstance == null) {
+            throw new NullPointerException("please inherit BaseApplication or call setApplication.");
+        }
+        return sInstance;
+    }
+}
